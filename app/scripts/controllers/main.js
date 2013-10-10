0dda4ab6c8a1a20ca7205222c075f1416efdc010
edwinhermawan <edwin@signaturepages.com>
2013-10-10 13:56:02 -0700

    Commit before Lorin craziness

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,14 +2,40 @@
 
 angular.module('tictacAngularApp')
   .controller('MainCtrl', ['$scope', 'angularFire', function($scope, angularFire){
+  		// Get Firebase fired up
   		var url = new Firebase("https://tictacPLEASEWORK.firebaseio.com/");
+  		
+  		// Initialize and link Firebase to queue
+  		var promise = angularFire(url, $scope, "queue", []);
+  		// on entry, push an item into the queue array
+  			// onload add someone to the queue
+  		promise.then(function(){
+  			$scope.queue.push({
+  				player1: true,
+  				player2: false,
+  			});
+  			console.log($scope.queue.length);
+  			console.log("at line 20")
+  		});
+  		
+  		// if (scope.queue.length%3 = 1){
+		// 	put in waiting  
+  		// }
+		// else if (scope.queue.length%3 = 2){
+		// 	enter game
+		// }
+		// else if (scope.queue.length%3 =0 ){
+		// 	start new game
+		// }
 
-  		var promise = angularFire(url, $scope, "ticTacToe");
+  		// Initialize and link ticTacToe to board
+  		$scope.ticTacToe = [];
+  		var promise = angularFire(url, $scope, "ticTacToe", []);
     // Controller for win condition
 		promise.then(function(){
 
 			console.log("entering 10");
-
+			
 			$scope.ticTacToe=[[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
 							  [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
 						      [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}]];
@@ -17,7 +43,7 @@ angular.module('tictacAngularApp')
 			$scope.classes = ['X', 'O'];
 			$scope.counter={value: 0};	
 			$scope.checkWin = function(row, column, holder){ 
-				console.log("entering alternating code")
+				console.log("entering alternating code");
 			// Make the X's and O's alternate
 				$scope.counter.value += 1;
 				holder.turn = $scope.counter.value;
@@ -37,7 +63,7 @@ angular.module('tictacAngularApp')
 					this.ticTacToe[1][x].value == this.ticTacToe[2][x].value && 
 					this.ticTacToe[1][x].value !== ""){
 					document.getElementById("popup").style.display="block";}
-				console.log("win condition code executes")
+					console.log("win condition code executes")
 				// alert(ticTacToe[0]{{x}} + " won in column " +( (x==)
 			
 				// if((row==x,column==0) == (row==x,column==1}}) &&
@@ -45,11 +71,11 @@ angular.module('tictacAngularApp')
 				// row({{x}},column{{0}}) != ""){
 				// document.getElementById("popup").style.display="block";}
 				// };
-			};
+				};
 
 			// $scope.ticTacToe.onDisconnect().remove()
 			// console.log( $scope.ticTacToe.onDisconnect().remove())
-		};
-	});
+			};
+		});
 	}]);
 
