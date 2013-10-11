f364ff591cb3d856afe11b6431901bec4b182f70
edwinhermawan <edwin@signaturepages.com>
2013-10-11 11:16:18 -0700

    Board for player2 is still not displaying on load but wanted to commit before trying some things out.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,49 +3,52 @@
 angular.module('tictacAngularApp')
   .controller('MainCtrl', ['$scope', 'angularFire', function($scope, angularFire){
   		// Get Firebase fired up
-  		$scope.games = [];
+  		
   		$scope.queue = {};
+  		$scope.games = [];
 
-  		var ticTacUrl = new Firebase("https://tictacPLEASEWORK.firebaseio.com/queue");
-  		angularFire(ticTacUrl, $scope, "queue", {}).then(function(){
+  		// Initialize and link Firebase to queue
+  		var queue = new Firebase("https://tictacPLEASEWORK.firebaseio.com/queue");
+  		angularFire(queue, $scope, "queue", {}).then(function(){
 
+  			// Initialize and link Firebase to games
 			var gamesUrl = new Firebase("https://tictacPLEASEWORK.firebaseio.com/games");
-	  		// Initialize and link Firebase to queue
 	  		angularFire(gamesUrl, $scope, "games", []).then( function(){ 
+
+	  			// Logic for assigning Player 1
 	  			if($scope.queue.gameId == undefined){
-	  				console.log("$scope.queue.gameId is " + $scope.queue.gameId);
 	  				$scope.player = "p1";
-	  				console.log("this is player" + $scope.player);
+	  				console.log("This is player " + $scope.player);
+
 	  				var newGame = {
 	  					board: [[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
 								[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
 							    [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}]]
 	  				};
-	  					console.log("instantiating newGame")
 	  				$scope.gameId = $scope.games.push(newGame) - 1;
-	  					console.log($scope.gameId);
-  					$scope.queue = {hello:"world"};
+	  				console.log("This is $scope.games " + $scope.games)
+	  				console.log("We are in gameId " + $scope.gameId)
 	  				$scope.queue.gameId = $scope.gameId;
-	  					console.log("this is the value of $scope.queue.gameId = " + $scope.queue.gameId);
-
+  					console.log("The value of $scope.queue.gameId = " + $scope.queue.gameId);
 	  			}
+
+	  			// Logic for assigning Player 2
 	  			else{
-	  				// Designate the player as Player 2
 	  				$scope.player = "p2";
-	  				console.log("this is player" + $scope.player);
+	  				console.log("This is player " + $scope.player);
 	  				// Designate which game it's in
 	  				$scope.gameId = $scope.queue.gameId;
-					// clear out the queue
+	  				console.log($scope.games)
+	  				console.log("We are in gameId " + $scope.gameId)
+					// Clear out the queue
 	  				$scope.queue = {};
 	  			}
-  	
-    // Controller for win condition
-			// $scope.ticTacToe=[[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
-			// 				  [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
-			// 			      [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}]];
-	
+  			
 			$scope.classes = ['X', 'O'];
 			$scope.counter={value: 0};	
+
+			console.log("Got to $scope.counter")
+
 			$scope.checkWin = function(row, column, holder){ 
 				console.log("entering alternating code");
 			// Make the X's and O's alternate
@@ -58,17 +61,16 @@ angular.module('tictacAngularApp')
 			// Win condition
 				console.log("entering win condition code")
 				for(var x=0;x<=2;x+=1){
-					if(this.games[x][0].value == this.games[x][1].value &&
-					this.games[x][1].value == this.games[x][2].value && 
-					this.games[x][1].value !== ""){
+					if($scope.games[gameId][x][0].value == $scope.games[gameId][x][1].value &&
+					$scope.games[gameId][x][1].value == $scope.games[gameId][x][2].value && 
+					$scope.games[gameId][x][1].value !== ""){
 					document.getElementById("popup").style.display="block";}
 				
-					if(this.games[0][x].value == this.games[1][x].value &&
-					this.games[1][x].value == this.games[2][x].value && 
-					this.games[1][x].value !== ""){
+					if($scope.games[gameId][0][x].value == $scope.games[gameId][1][x].value &&
+					$scope.games[gameId][1][x].value == $scope.games[gameId][2][x].value && 
+					$scope.games[gameId][1][x].value !== ""){
 					document.getElementById("popup").style.display="block";}
 					console.log("win condition code executes")
-				// alert(ticTacToe[0]{{x}} + " won in column " +( (x==)
 			
 				// if((row==x,column==0) == (row==x,column==1}}) &&
 				// row({{x}},column{{1}}) == row({{x}},columncolumn{{2}}) && 
