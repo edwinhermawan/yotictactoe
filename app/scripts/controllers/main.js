08a58cbd6731aec7acdc88e8816ac0563374b135
edwinhermawan <edwin@signaturepages.com>
2013-10-10 21:53:19 -0700

    Got P1 and P2 to show up but board only loads on first click for P2

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,45 +3,46 @@
 angular.module('tictacAngularApp')
   .controller('MainCtrl', ['$scope', 'angularFire', function($scope, angularFire){
   		// Get Firebase fired up
-  		$scope.ticTacToe = [];
+  		$scope.games = [];
   		$scope.queue = {};
 
-		var queueUrl = new Firebase("https://tictacPLEASEWORK.firebaseio.com/queue");
-  		// Initialize and link Firebase to queue
-  		angularFire(queueUrl, $scope, "queue").then( function(){ 
-  			if($scope.queue.gameId == undefined){
-  				console.log("$scope.queue.gameId is " + $scope.queue.gameId)
-  				$scope.player = 1
-  				console.log("this is player" + $scope.player)
-  				var newGame ={
-  					board: [[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
-							   [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
-						       [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}]]
+  		var ticTacUrl = new Firebase("https://tictacPLEASEWORK.firebaseio.com/queue");
+  		angularFire(ticTacUrl, $scope, "queue", {}).then(function(){
 
-  				}
+			var gamesUrl = new Firebase("https://tictacPLEASEWORK.firebaseio.com/games");
+	  		// Initialize and link Firebase to queue
+	  		angularFire(gamesUrl, $scope, "games", []).then( function(){ 
+	  			if($scope.queue.gameId == undefined){
+	  				console.log("$scope.queue.gameId is " + $scope.queue.gameId);
+	  				$scope.player = "p1";
+	  				console.log("this is player" + $scope.player);
+	  				var newGame = {
+	  					board: [[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
+								[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
+							    [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}]]
+	  				};
+	  					console.log("instantiating newGame")
+	  				$scope.gameId = $scope.games.push(newGame) - 1;
+	  					console.log($scope.gameId);
+  					$scope.queue = {hello:"world"};
+	  				$scope.queue.gameId = $scope.gameId;
+	  					console.log("this is the value of $scope.queue.gameId = " + $scope.queue.gameId);
 
-  				$scope.gameId = $scope.ticTacToe.push(newGame) - 1
-  				$scope.queue.gameId = $scope.gameId
-  			}
-  			else{
-  				// Designate the player as Player 2
-  				$scope.player = 2
-  				console.log("this is player" + $scope.player)
-  				// Designate which game it's in
-  				$scope.gameId = $scope.queue.gameId
-				// clear out the queue
-  				$scope.queue = {}
-  			}
-  		
-  		// on entry, push an item into the queue array
-  			// onload add someone to the queue
-  		// Initialize and link ticTacToe to board
-  		var ticTacUrl = new Firebase("https://tictacPLEASEWORK.firebaseio.com/ticTacToe");
-  			angularFire(ticTacUrl, $scope, "ticTacToe", []).then(function(){;
+	  			}
+	  			else{
+	  				// Designate the player as Player 2
+	  				$scope.player = "p2";
+	  				console.log("this is player" + $scope.player);
+	  				// Designate which game it's in
+	  				$scope.gameId = $scope.queue.gameId;
+					// clear out the queue
+	  				$scope.queue = {};
+	  			}
+  	
     // Controller for win condition
-			$scope.ticTacToe=[[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
-							  [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
-						      [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}]];
+			// $scope.ticTacToe=[[{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
+			// 				  [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}], 
+			// 			      [{value: '', turn: ''},{value: '', turn: ''},{value: '', turn: ''}]];
 	
 			$scope.classes = ['X', 'O'];
 			$scope.counter={value: 0};	
@@ -57,14 +58,14 @@ angular.module('tictacAngularApp')
 			// Win condition
 				console.log("entering win condition code")
 				for(var x=0;x<=2;x+=1){
-					if(this.ticTacToe[x][0].value == this.ticTacToe[x][1].value &&
-					this.ticTacToe[x][1].value == this.ticTacToe[x][2].value && 
-					this.ticTacToe[x][1].value !== ""){
+					if(this.games[x][0].value == this.games[x][1].value &&
+					this.games[x][1].value == this.games[x][2].value && 
+					this.games[x][1].value !== ""){
 					document.getElementById("popup").style.display="block";}
 				
-					if(this.ticTacToe[0][x].value == this.ticTacToe[1][x].value &&
-					this.ticTacToe[1][x].value == this.ticTacToe[2][x].value && 
-					this.ticTacToe[1][x].value !== ""){
+					if(this.games[0][x].value == this.games[1][x].value &&
+					this.games[1][x].value == this.games[2][x].value && 
+					this.games[1][x].value !== ""){
 					document.getElementById("popup").style.display="block";}
 					console.log("win condition code executes")
 				// alert(ticTacToe[0]{{x}} + " won in column " +( (x==)
@@ -75,10 +76,8 @@ angular.module('tictacAngularApp')
 				// document.getElementById("popup").style.display="block";}
 				// };
 				};
-
-			// $scope.ticTacToe.onDisconnect().remove()
-			// console.log( $scope.ticTacToe.onDisconnect().remove())
 			};
 		});
-	});
-}])
+});
+}
+])
